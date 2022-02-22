67e3d86b74bc3b39f675dece9cc7b12e196a5d7c
mlgrespan <matheus.grespan@gmail.com>
2022-02-22 16:43:19 -0300

    Primeiros passos Angular

--- src/app/courses/course-list-component.ts
+++ src/app/courses/course-list-component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-course-list',
-    template: '<h2> Course List </h2>'
+    selector: 'app-course-list', 
+    templateUrl: './course-list-component.html'
   })
 
 export class CourseListComponent {
