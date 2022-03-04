7f4dae438db3f0d95b2988d2d0a862d35c938d84
mlgrespan <matheus.grespan@gmail.com>
2022-03-04 17:22:35 -0300

    Angular

--- src/app/courses/course-list-component.ts
+++ src/app/courses/course-list-component.ts
@@ -8,18 +8,27 @@ import { Course } from './course';
 
 export class CourseListComponent implements OnInit {
 
-      courses: Course[] = [];
-      ngOnInit(): void {
+    courses: Course[] = [];
+        ngOnInit(): void {
         this.courses = [
-          {
-            id: 1,
-            name: 'Angular: Forms',
-            imageUrl: '',
-            price: 99.99,
-            code: 'XPS-8796',
-            duration: 120,
-            rating: 5.4,
-          }
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageUrl: '',
+                price: 99.99,
+                code: 'XPS-8796',
+                duration: 120,
+                rating: 4.5,
+            },
+            {
+                id: 2,
+                name: 'Angular: HTTP',
+                imageUrl: '',
+                price: 49.99,
+                code: 'LKL-1094',
+                duration: 80,
+                rating: 4,
+            }
         ]
     }
 }
