8bfc8186087632061fdbf5fd963ba6d3ba6727b5
mlgrespan <matheus.grespan@gmail.com>
2022-03-16 17:25:29 -0300

    Progetando Rotas com Guards

--- src/app/courses/course-list-component.ts
+++ src/app/courses/course-list-component.ts
@@ -23,8 +23,7 @@ export class CourseListComponent implements OnInit {
     }
 
     set filter(value: string) {
-        this._filterBy = value;
-        console.log(value);
+        this._filterBy = value;        
 
         this.filteredCourses = this._courses.filter((course: Course) => 
         course.name!.toLocaleLowerCase().indexOf(this._filterBy!.toLocaleLowerCase()) > -1);
